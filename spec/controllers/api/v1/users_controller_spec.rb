cd24c0426291bf5aad28fe4de6ec0710a3438859
Jason Watson <jwatson@newerahd.com>
2015-05-28 10:24:59 -0500

    end chapter 3

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -81,7 +81,24 @@ describe Api::V1::UsersController do
         expect(user_response[:errors][:email]).to include "is invalid"
       end
 
+      it "renders the json errors on whye the user could not be created" do
+        user_response = JSON.parse(response.body, :symbolize_names => true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+
       it {should respond_with 422}
+    end
+  end
+
+  describe "DELETE #destroy" do
+
+    context "When a user is deleted" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        delete :destroy, {:id => @user.id}, :format => :json
+      end
+
+      it {should respond_with 204}
     end
   end
 end
